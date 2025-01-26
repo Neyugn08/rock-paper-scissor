2954460fa87740bc7441b96fdb79d71af580e694
Neyugn <vukhangnguyen08@gmail.com>
2025-01-27 00:00:36 +0700

    Completing the game after 2h!

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Generating computer outputs
 function generater_choice() {
     let random = Math.floor(Math.random() * 10);
-    let a = random % 3;
+    let a = random * 13 % 3;
     let comp_choice;
     // 1, 0, -1 represent rock, paper, scissor respectively.
     if (a == 0) {
@@ -16,68 +16,112 @@ function generater_choice() {
     return comp_choice;
 }
 
-// Handling user inputs
-function user_input() {
-    let s = prompt("Type your choice: ");
-    let user_choice = s.toUpperCase();
-    if (user_choice != "ROCK" && user_choice != "PAPER" && user_choice != "SCISSOR") {
-        alert("Invalid input.");
-        return user_input();
+// The main code
+function winner(user_choice) {
+    round++;
+    rounds.textContent = round;
+    const MAX = 5;
+    let det = user_choice;
+    let sum = det + generater_choice();
+    
+    if (2 * det == sum) {
+        warning.textContent = "";
+        warning.textContent = "Tie!";
+        warning.style.fontSize = "30";
+        warning.style.fontWeight = "700";
+    }
+    else if (sum == 0) {
+        if (det == 1) {
+            counter++;
+            warning.textContent = "";
+            warning.textContent = "Rock beats scissor!";
+            warning.style.fontSize = "30";
+            warning.style.fontWeight = "700";
+        }
+        else {
+            counter--;
+            warning.textContent = "";
+            warning.textContent = "Rock beats scissor!";
+            warning.style.fontSize = "30";
+            warning.style.fontWeight = "700";
+        }
+    }   
+    else if (sum == 1) {
+        if (det == 0) {
+            counter++;
+            warning.textContent = "";
+            warning.textContent = "Paper beats rock!";
+            warning.style.fontSize = "30";
+            warning.style.fontWeight = "700";
+        }
+        else {
+            counter--;
+            warning.textContent = "";
+            warning.textContent = "Paper beats rock!";
+            warning.style.fontSize = "30";
+            warning.style.fontWeight = "700";
+        }
+    }  
+    else if (sum == -1) {
+        if (det == -1) {
+            counter++;
+            warning.textContent = "";
+            warning.textContent = "Scissor beats paper!";
+            warning.style.fontSize = "30";
+            warning.style.fontWeight = "700";
+        }
+        else {
+            counter--;
+            counter++;
+            warning.textContent = "";
+            warning.textContent = "Scissor beats paper!";
+            warning.style.fontSize = "30";
+            warning.style.fontWeight = "700";
+        }
+    }
+
+    console.log(counter);
+    
+    if (round === MAX) {
+        if (counter > 0) {console.log("You are the winner!");}
+        else if (counter < 0)  {console.log("You are the loser!");}
+        else {console.log("It is a tie");}
+        round = 0;
+        counter = 0;
+    }
+}
+
+const container = document.querySelector(".container");
+const body = document.querySelector("body");
+let warning = document.querySelector(".warning");
+let rounds = document.querySelector(".rounds");
+let again = document.querySelector(".again");
+
+let counter = 0;
+let round = 0;
+
+container.addEventListener("click", function(e) {
+    let user_choice = e.target.className.toUpperCase();
+    if (user_choice !== "ROCK" && user_choice !== "PAPER" && user_choice !== "SCISSOR") {
+        /*const warning = document.createElement("div");
+        warning.textContent = "You clicked the wrong button!";
+        warning.style.color = "red";
+        warning.style.fontSize = "30";
+        warning.style.fontWeight = "700";
+        container.insertBefore(warning, rockBtn);*/
     }
     else {
-        if (user_choice[0] == "R") return 1;
-        else if (user_choice[0] == "S") return -1;
-        else if (user_choice[0] == "P") return 0;
+        let ans;
+        if (user_choice[0] == "R") ans = 1;
+        else if (user_choice[0] == "S") ans = -1;
+        else if (user_choice[0] == "P") ans = 0;
+        winner(ans);
     }
-}
+});
 
-// The main code
-function winner() {
-    let counter = 0;
-    const MAX = 5;
-    for (i = 0; i < MAX; i++) {
-        let det = user_input();
-        let sum = det + generater_choice();
-        // determining tie
-        if (2 * det == sum) {
-            alert("Tie!");
-            continue;
-        }
-
-        if (sum == 0) {
-            if (det == 1) {
-                counter++;
-                alert("Rock beats scissor!");
-            }
-            else {
-                counter--;
-                alert("Rock beats scissor!");
-            }
-        }   
-        else if (sum == 1) {
-            if (det == 0) {
-                counter++;
-                alert("Paper beats rock!");
-            }
-            else {
-                counter--;
-                alert("Paper beats rock!");
-            }
-        }  
-        else {
-            if (det == -1) {
-                counter++;
-                alert("Scissor beats paper!");
-            }
-            else {
-                counter--;
-                alert("Scissor beats paper!");
-            }
-        }
-    }
-    if (counter > 0) {alert("You are the winner!");}
-    else if (counter < 0)  {alert("You are the loser!");}
-    else {alert("It is a tie");}
-}
-
-winner();
+again.addEventListener("click", () => {
+    round = 0; 
+    counter = 0;
+    warning.textContent = "";
+    rounds.textContent = "";
+});
